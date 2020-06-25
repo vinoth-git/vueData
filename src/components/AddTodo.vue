23f3354c15617094ccbd62f4771b8311897f03e9
vinoth740 <svinothkumar740@gmail.com>
2020-06-25 15:23:11 +0530

    edited

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+    <div>
+        <form>
+            <input type="text" name="title" placeholder="Add ToDo">
+            <input type="submit" value="submit" class="btn">
+        </form>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "AddTodo"
+}
+</script>
+
+
+<style scoped>
+    form {
+        display: flex;
+    }
+    input[type="text"] {
+        flex: 10;
+        padding: 5px;
+    }
+    input[type="submit"] {
+        flex: 2;
+    }
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
